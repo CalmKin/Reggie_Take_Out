30c192008d31ffc2d261f657630e7960dcbaa453
calmkin <490363906@qq.com>
2023-01-23 20:58:43 +0800

    添加了项目的基本依赖、项目配置文件，并将前端资源移动到相关文件夹下

---
+++ src/main/java/com/calmkin/ReggieTakeOutApplication.java
@@ -0,0 +1,13 @@
+package com.calmkin;
+
+import org.apache.juli.logging.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ReggieTakeOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieTakeOutApplication.class, args);
+    }
+
+}
