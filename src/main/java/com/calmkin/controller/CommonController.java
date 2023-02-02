dff43ca0465c890e633fad2382c774b401dfe28a
calmkin <490363906@qq.com>
2023-02-02 11:24:16 +0800

    完成了图片上传的回显功能

--- src/main/java/com/calmkin/controller/CommonController.java
+++ src/main/java/com/calmkin/controller/CommonController.java
@@ -3,13 +3,16 @@ package com.calmkin.controller;
 import com.calmkin.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.UUID;
 
 @Slf4j
@@ -38,5 +41,43 @@ public class CommonController {
         }
         //将文件名返回给前端，方便在数据库中存储文件名
         return R.success(fileName);
+    }
+
+    //进行图片回显,请求路径:请求网址: http://localhost:8080/common/download?name=xxx.jpg
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response)
+    {
+        //获取文件输入流
+        try {
+            FileInputStream inputStream = new FileInputStream(filePath+name);
+
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            //别忘记设置一下内容格式，否则浏览器不知道怎么去解析
+            response.setContentType("image/jpeg");
+
+            //将文件写入到请求的输出流当中
+            byte[] arr = new byte[1024];
+            int len=0;
+
+            while ( (len = inputStream.read(arr) ) != -1 )  //一直从文件输入流里面读取arr数组那么多个字节，直到不能读为止
+            {
+
+                //就向输出流里面写入那么多数据
+                outputStream.write(arr,0,len);
+                //清空缓冲区
+                outputStream.flush();
+            }
+
+            //别忘记关闭资源
+            outputStream.close();
+            inputStream.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
     }
 }
