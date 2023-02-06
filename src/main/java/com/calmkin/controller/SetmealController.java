837e562602b3bc35767592810d9e9f6959d6075d
calmkin <490363906@qq.com>
2023-02-06 09:38:09 +0800

    完成套餐信息分页查询

--- src/main/java/com/calmkin/controller/SetmealController.java
+++ src/main/java/com/calmkin/controller/SetmealController.java
@@ -1,14 +1,20 @@
 package com.calmkin.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.calmkin.common.R;
 import com.calmkin.dto.SetmealDto;
+import com.calmkin.pojo.Category;
+import com.calmkin.pojo.Setmeal;
+import com.calmkin.service.CategoryService;
 import com.calmkin.service.SetmealDishService;
 import com.calmkin.service.SetmealService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/setmeal")
@@ -17,11 +23,46 @@ public class SetmealController {
     private SetmealDishService setmealDishService;
     @Autowired
     private SetmealService setmealService;
+
+    @Autowired
+    private CategoryService categoryService;
+
     @PostMapping
     public R<String> uploadWithDishes(@RequestBody SetmealDto setmealDto)
     {
         setmealService.saveWithDishes(setmealDto);
         return R.success("添加套餐成功");
+    }
+
+
+    @GetMapping("/page")
+    public R<Page> getPage(int page,int pageSize,String name)
+    {
+        Page<Setmeal> setmealPage = new Page<>(page,pageSize);
+        Page<SetmealDto> setmealDtoPage = new Page<>();
+
+        LambdaQueryWrapper<Setmeal> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        setmealService.page(setmealPage,lambdaQueryWrapper);
+
+        List<Setmeal> records = setmealPage.getRecords();
+
+        List<SetmealDto> lists = records.stream().map((item)->{
+            //进行属性拷贝
+            SetmealDto setmealDto = new SetmealDto();
+            BeanUtils.copyProperties(item,setmealDto);
+            //查询这个套餐对应的分类名称
+            Long categoryId = item.getCategoryId();
+            Category category = categoryService.getById(categoryId);
+            setmealDto.setCategoryName(category.getName());
+
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        BeanUtils.copyProperties(setmealPage,setmealDtoPage,"records");
+
+        setmealDtoPage.setRecords(lists);
+
+        return R.success(setmealDtoPage);
     }
 
 }
