44477289c787f3d82e36f9594a67c430a362921c
calmkin <490363906@qq.com>
2023-03-02 20:32:27 +0800

    完成历史订单展示模块

--- src/main/java/com/calmkin/controller/OrderController.java
+++ src/main/java/com/calmkin/controller/OrderController.java
@@ -1,14 +1,25 @@
 package com.calmkin.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.calmkin.common.BaseContext;
 import com.calmkin.common.R;
+import com.calmkin.dto.OrdersDto;
+import com.calmkin.pojo.OrderDetail;
 import com.calmkin.pojo.Orders;
+import com.calmkin.service.OrderDetailService;
 import com.calmkin.service.OrdersService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/order")
@@ -16,6 +27,9 @@ public class OrderController {
 
     @Autowired
     private OrdersService ordersService;
+
+    @Autowired
+    private OrderDetailService detailService;
 
     /**
      * 提交订单进行支付
@@ -30,5 +44,50 @@ public class OrderController {
         return R.success("支付成功");
         else
             return R.error("支付失败");
+    }
+
+    /**
+     * 请求网址: http://localhost:8080/order/userPage?page=1&pageSize=1
+     *
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/userPage")
+    public R<Page> allOrders(int page, int pageSize)
+    {
+
+        Page<OrdersDto> page1 = new Page<>(page,pageSize);
+        Page<Orders> page2 = new Page<>(page,pageSize);
+
+        Long userId = BaseContext.getID();
+
+        //找到这个用户的所有订单,进行分页查询
+        LambdaQueryWrapper<Orders> ordersLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        ordersLambdaQueryWrapper.eq(Orders::getUserId,userId);
+        ordersService.page(page2,ordersLambdaQueryWrapper);
+        //分页查询属性值拷贝
+        BeanUtils.copyProperties(page2,page1,"records");
+
+        List<Orders> list = page2.getRecords();
+
+        List<OrdersDto> records2 = list.stream().map((item) -> {
+            OrdersDto dto = new OrdersDto();
+            BeanUtils.copyProperties(item, dto);
+
+            Long orderId = item.getId();
+
+            //获取该订单下的所有子订单
+            LambdaQueryWrapper<OrderDetail> detail = new LambdaQueryWrapper<>();
+            detail.eq(OrderDetail::getOrderId, orderId);
+            List<OrderDetail> detailList = detailService.list(detail);
+            dto.setList(detailList);
+            return dto;
+        }).collect(Collectors.toList());
+
+
+        page1.setRecords(records2);
+
+        return R.success(page1);
     }
 }
---
+++ src/main/java/com/calmkin/dto/OrdersDto.java
@@ -0,0 +1,23 @@
+package com.calmkin.dto;
+
+import com.calmkin.pojo.OrderDetail;
+import com.calmkin.pojo.Orders;
+import lombok.Data;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+@Data
+public class OrdersDto extends Orders {
+
+/**
+ * orderTime
+ * status
+ * + orderDetails(index,dishDetail)
+ * + sumNum
+ * amount
+ */
+   private List<OrderDetail> list;
+}
